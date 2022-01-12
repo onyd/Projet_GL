4650b068c2e30ef31c7166f8d8fbca13bddfd1a8
Anthony Dard <anthony.dard@grenoble-inp.org>
2022-01-12 15:12:24 +0100

    Implementation of a LabelManger
    Add getNextLabel for className and static field for generic error Label.

--- src/main/java/fr/ensimag/deca/codegen/LabelManager.java
+++ src/main/java/fr/ensimag/deca/codegen/LabelManager.java
@@ -9,6 +9,10 @@ import java.util.Map;
 public class LabelManager {
     private HashMap<String, Integer> labelsOccurrences = new HashMap<>();
 
+    public static final Label STACK_OVERFLOW_ERROR = new Label("stack_overflow_error"),
+            OVERFLOW_ERROR = new Label("overflow_error"),
+            IO_ERROR = new Label("io_error");
+
     public Label getNextLabel(String labelName) {
         Integer occurrences = labelsOccurrences.get(labelName);
         if (occurrences != null) {
@@ -18,6 +22,10 @@ public class LabelManager {
             labelsOccurrences.put(labelName, 0);
             return new Label(labelName + ".0");
         }
+    }
+
+    public Label getNextLabel(String labelName, String className) {
+        return getNextLabel(labelName + "." + className);
     }
 
 
