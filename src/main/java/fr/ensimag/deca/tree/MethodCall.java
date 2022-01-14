fc03cd49dc599e4ae0621781b26c1133258e536f
Martin Gangand <martin.gangand@grenoble-inp.org>
2022-01-14 17:49:56 +0100

    Création class MethodCall et Selection pour parser avec objet, et début parser avec objet

---
+++ src/main/java/fr/ensimag/deca/tree/MethodCall.java
@@ -0,0 +1,43 @@
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+
+import java.io.PrintStream;
+
+public class MethodCall extends AbstractExpr {
+    private AbstractIdentifier identifierLeft;
+    private AbstractIdentifier identifierRight;
+    private ListExpr arguments;
+
+    public MethodCall(AbstractIdentifier identifierLeft, AbstractIdentifier identifierRight, ListExpr arguments) {
+        super();
+        this.identifierLeft = identifierLeft;
+        this.identifierRight = identifierRight;
+        this.arguments = arguments;
+    }
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
+        return null;
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+
+    }
+}
