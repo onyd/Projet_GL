9ae16ee6aed746ff31f61b36160f2f3e7760a941
Basel <baselinp@gmail.com>
2022-01-17 16:13:39 +0100

    premier gencodebyte

--- src/main/java/fr/ensimag/deca/JavaCompiler.java
+++ src/main/java/fr/ensimag/deca/JavaCompiler.java
@@ -1,5 +1,32 @@
 package fr.ensimag.deca;
 
-public class JavaCompiler
+import org.objectweb.asm.*;
+
+public class JavaCompiler implements Opcodes
 {
+    // A voir si il faut les déclarer statique ou cela pose problème pour le parallisme.
+    private ClassWriter classWriter = new ClassWriter(0);
+    private FieldVisitor fieldVisitor;
+    private MethodVisitor methodVisitor;
+    private AnnotationVisitor annotationVisitor;
+
+    public ClassWriter getClassWriter()
+    {
+        return classWriter;
+    }
+
+    public FieldVisitor getFieldVisitor()
+    {
+        return fieldVisitor;
+    }
+
+    public MethodVisitor getMethodVisitor()
+    {
+        return methodVisitor;
+    }
+
+    public AnnotationVisitor getAnnotationVisitor()
+    {
+        return annotationVisitor;
+    }
 }
