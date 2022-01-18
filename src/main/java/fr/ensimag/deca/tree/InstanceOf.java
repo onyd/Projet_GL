5efb78e5ab6f3653e957e69586a3abc9a0d6e569
Anthony Dard <anthony.dard@grenoble-inp.org>
2022-01-18 15:11:25 +0100

    Correction of Selection with public/protected field

--- src/main/java/fr/ensimag/deca/tree/InstanceOf.java
+++ src/main/java/fr/ensimag/deca/tree/InstanceOf.java
@@ -40,11 +40,9 @@ public class InstanceOf extends AbstractExpr {
         ClassType type1 = expr.verifyExpr(compiler, localEnv, currentClass).asClassType("(3.40) Left side of instanceof must have class type", getLocation());
         ClassType type2 = type.verifyType(compiler).asClassType("(3.40) Right side of instanceof must be a class identifier", getLocation());
 
-        if (type2.isSubClassOf(type1) || type1.isNull()) {
+        if (type2.isClass() && type1.isClassOrNull()) {
             setType(compiler.getEnvironmentType().get(compiler.BOOLEAN_SYMBOL).getType());
         }
-
-        // TODO finish 3.40
 
         return getType();
     }
