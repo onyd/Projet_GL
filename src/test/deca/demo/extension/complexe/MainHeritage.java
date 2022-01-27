ccb03a8678b9dafff9b79811bd3c730358ad104b
Basel <baselinp@gmail.com>
2022-01-27 06:59:48 +0100

    Maj demo heritage

--- src/test/deca/demo/extension/complexe/MainHeritage.java
+++ src/test/deca/demo/extension/complexe/MainHeritage.java
@@ -4,9 +4,12 @@ public class MainHeritage
     {
         Ensimag e = new Ensimag();
         e.setNbEleve(1000);
+        e.setBudget(3000000);
+
         if(e.estDansINP()) {
             System.out.println("Cette école est bien dans l'INP");
         }
         System.out.println("le nombre d'élèves à l'ensimag est de " + e.getNbEleve());
+        System.out.println("le budget de l'ensimag est " + e.getBudget() + " euros");
     }
 }
